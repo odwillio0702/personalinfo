d234e975aad9747ff7d8fd163e3dcbc72e4a6fcb
odwillio0702 <odwillio21100@gmail.com>
2025-12-15 23:28:15 +0100

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,6 +1,16 @@
 from telebot import types
 
+# Кнопка "Я сделал"
 def done_keyboard():
-    kb = types.InlineKeyboardMarkup()
+    kb = types.InlineKeyboardMarkup(row_width=1)
     kb.add(types.InlineKeyboardButton("✅ Я сделал", callback_data="done"))
+    return kb
+
+# Кнопки "Я сделал" + "Отложить 10 минут"
+def done_delay_keyboard():
+    kb = types.InlineKeyboardMarkup(row_width=2)
+    kb.add(
+        types.InlineKeyboardButton("✅ Я сделал", callback_data="done"),
+        types.InlineKeyboardButton("⏰ Отложить на 10 минут", callback_data="delay10")
+    )
     return kb
