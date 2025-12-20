07cd8106b5d515f1ad365b74d2fe558cef37bd37
odwillio0702 <odwillio21100@gmail.com>
2025-12-20 23:11:19 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,3 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+WEBAPP_URL = os.getenv(
+    "WEBAPP_URL",
+    "https://odwillio0702.github.io/personalinfo/"
+)
